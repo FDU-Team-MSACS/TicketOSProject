53d8fe1b85e15b92e5013ac739010acd7826d488
Wang <wzzwzzz2@gmail.com>
2024-07-06 15:01:39 -0700

    Relocate to main

---
+++ testFile_1.java
@@ -0,0 +1,7 @@
+// For VScode Git update test purpose.
+
+public class testFile_1 {
+    public static void main(String[] args) {
+        System.out.println("We are Global Kernel Pioneers Team ");
+    }
+}
